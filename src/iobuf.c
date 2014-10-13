eb5693cb4723604d97d1ff05e241bfbe710e1b27
Martin Hurton <hurtonm@gmail.com>
2014-10-13 16:35:09 +0200

    Add iobuf_t class

---
+++ src/iobuf.c
@@ -0,0 +1,42 @@
+// I/O buffer class
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "iobuf.h"
+
+iobuf_t *
+iobuf_new (uint8_t *data, size_t size)
+{
+    iobuf_t *self = (iobuf_t *) malloc (sizeof *self);
+    if (self)
+        iobuf_init (self, data, size);
+    return self;
+}
+
+void
+iobuf_destroy (iobuf_t **self_p)
+{
+    assert (self_p);
+    if (*self_p) {
+        iobuf_t *self = *self_p;
+        free (self);
+        *self_p = NULL;
+    }
+}
+
+extern inline void
+iobuf_init (iobuf_t *self, uint8_t *base, size_t size);
+
+extern inline bool
+iobuf_is_empty (iobuf_t *self);
+
+extern inline size_t
+iobuf_available (iobuf_t *self);
+
+extern inline size_t
+iobuf_space (iobuf_t *self);
+
+extern inline size_t
+iobuf_read (iobuf_t *self, void *ptr, size_t n);
---
+++ src/iobuf.h
@@ -0,0 +1,59 @@
+//  I/O buffer class
+
+#ifndef __IO_BUFFER_H_INCLUDED__
+#define __IO_BUFFER_H_INCLUDED__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+struct iobuf {
+    uint8_t *base;
+    size_t size;
+    uint8_t *r;
+    uint8_t *w;
+};
+
+typedef struct iobuf iobuf_t;
+
+iobuf_t *
+    iobuf_new (uint8_t *base, size_t size);
+
+void
+    iobuf_destroy (iobuf_t **self_p);
+
+inline void
+iobuf_init (iobuf_t *self, uint8_t *base, size_t size)
+{
+    *self = (iobuf_t) { .base = base, .size = size, .r = base, .w = base };
+}
+
+inline bool
+iobuf_is_empty (iobuf_t *self)
+{
+    return self->r == self->w;
+}
+
+inline size_t
+iobuf_available (iobuf_t *self)
+{
+    return (size_t) (self->w - self->r);
+}
+
+inline size_t
+iobuf_space (iobuf_t *self)
+{
+    return (size_t) (self->base + self->size - self->w);
+}
+
+inline size_t
+iobuf_read (iobuf_t *self, void *ptr, size_t n)
+{
+    const size_t available = iobuf_available (self);
+    if (n > available)
+        n = available;
+    memcpy (ptr, self->r, n);
+    self->r += n;
+    return n;
+}
+
+#endif
