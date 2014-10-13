f70817c28e1aed1b3e03368252799b024feb0a0a
Martin Hurton <hurtonm@gmail.com>
2014-10-13 16:53:29 +0200

    Add frame_t class

---
+++ src/frame.c
@@ -0,0 +1,29 @@
+//  Frame class
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include "zkernel.h"
+#include "frame.h"
+
+frame_t *
+frame_new ()
+{
+    frame_t *frame = (frame_t *) malloc (sizeof *frame);
+    if (frame) {
+        frame->base = (msg_t) { .msg_type = ZKERNEL_MSG_TYPE_FRAME };
+        frame->frame_size = 0;
+    }
+    return frame;
+}
+
+void
+frame_destroy (frame_t **self_p)
+{
+    assert (self_p);
+    if (*self_p) {
+          frame_t *self = *self_p;
+          free (self);
+          *self_p = NULL;
+    }
+}
---
+++ src/frame.h
@@ -0,0 +1,24 @@
+//  Frame class
+
+#ifndef __FRAME_H_INCLUDED__
+#define __FRAME_H_INCLUDED__
+
+#include <stdint.h>
+
+#include "msg.h"
+
+struct frame {
+    msg_t base;
+    size_t frame_size;
+    uint8_t frame_data [64];
+};
+
+typedef struct frame frame_t;
+
+frame_t *
+    frame_new ();
+
+void
+    frame_destroy (frame_t **self_p);
+
+#endif
