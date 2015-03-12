294e0278f1b0485f723d0b99de6bc18457fc749a
Martin Hurton <hurtonm@gmail.com>
2015-03-12 17:53:46 +0100

    zmtp_v3_encoder: Do not extend encoder class

--- src/zmtp_v3_encoder.c
+++ src/zmtp_v3_encoder.c
@@ -7,45 +7,41 @@
 
 #include "iobuf.h"
 #include "pdu.h"
-#include "encoder.h"
+#include "zmtp_v3_encoder.h"
 
 #define WAITING_FOR_PDU     0
 #define READING_HEADER      1
 #define READING_BODY        2
 
 struct zmtp_v3_encoder {
-    encoder_t base;
     int state;
+    size_t buffer_threshold;
     uint8_t buffer [9];
     pdu_t *pdu;
     uint8_t *ptr;
     size_t bytes_left;
 };
 
-typedef struct zmtp_v3_encoder zmtp_v3_encoder_t;
-
 static void
 put_uint64 (uint8_t *ptr, uint64_t n);
 
-static struct encoder_ops encoder_ops;
-
 zmtp_v3_encoder_t *
-s_new ()
+zmtp_v3_encoder_new ()
 {
     zmtp_v3_encoder_t *self =
         (zmtp_v3_encoder_t *) malloc (sizeof *self);
     if (self)
         *self = (zmtp_v3_encoder_t) {
-            .base = (encoder_t) { .ops = encoder_ops },
-            .state = WAITING_FOR_PDU
+            .state = WAITING_FOR_PDU,
+            .buffer_threshold = 4096,
         };
     return self;
 }
 
-static int
-s_encode (encoder_t *base, pdu_t *pdu, encoder_status_t *status)
+int
+zmtp_v3_encoder_putmsg (
+    zmtp_v3_encoder_t *self, pdu_t *pdu, zmtp_v3_encoder_status_t *status)
 {
-    zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) base;
     assert (self);
 
     if (self->state != WAITING_FOR_PDU)
@@ -66,14 +62,17 @@ s_encode (encoder_t *base, pdu_t *pdu, encoder_status_t *status)
         self->bytes_left = 2;
     }
 
-    *status = ZKERNEL_ENCODER_READ_OK;
+    *status = ZMTP_V3_ENCODER_READ_OK;
+    if (self->bytes_left >= self->buffer_threshold)
+        *status |= ZMTP_V3_ENCODER_BUFFER_FLAG;
+
     return 0;
 }
 
-static int
-s_read (encoder_t *base, iobuf_t *iobuf, encoder_status_t *status)
+int
+zmtp_v3_encoder_read (
+    zmtp_v3_encoder_t *self, iobuf_t *iobuf, zmtp_v3_encoder_status_t *status)
 {
-    zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) base;
     assert (self);
 
     if (self->state == WAITING_FOR_PDU)
@@ -108,19 +107,21 @@ s_read (encoder_t *base, iobuf_t *iobuf, encoder_status_t *status)
         }
     }
 
-    *status = self->bytes_left & ZKERNEL_ENCODER_BUFFER_MASK;
-    if (self->bytes_left)
-        *status |= ZKERNEL_ENCODER_READY;
-    else
-        *status |= ZKERNEL_ENCODER_READ_OK;
+    if (self->state == WAITING_FOR_PDU)
+        *status = ZMTP_V3_ENCODER_READY;
+    else {
+        *status = ZMTP_V3_ENCODER_READ_OK;
+        if (self->bytes_left >= self->buffer_threshold)
+            *status |= ZMTP_V3_ENCODER_BUFFER_FLAG;
+    }
 
     return 0;
 }
 
-static int
-s_buffer (encoder_t *base, const void **buffer, size_t *buffer_size)
+int
+zmtp_v3_encoder_buffer (
+    zmtp_v3_encoder_t *self, const void **buffer, size_t *buffer_size)
 {
-    zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) base;
     assert (self);
 
     if (self->state == WAITING_FOR_PDU) {
@@ -135,10 +136,10 @@ s_buffer (encoder_t *base, const void **buffer, size_t *buffer_size)
     return 0;
 }
 
-static int
-s_advance (encoder_t *base, size_t n, encoder_status_t *status)
+int
+zmtp_v3_encoder_advance (
+    zmtp_v3_encoder_t *self, size_t n, zmtp_v3_encoder_status_t *status)
 {
-    zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) base;
     assert (self);
 
     if (self->state == WAITING_FOR_PDU)
@@ -166,32 +167,37 @@ s_advance (encoder_t *base, size_t n, encoder_status_t *status)
     if (self->bytes_left == 0) {
         pdu_destroy (&self->pdu);
         self->state = WAITING_FOR_PDU;
-        *status = ZKERNEL_ENCODER_READY;
+        *status = ZMTP_V3_ENCODER_READY;
     }
-    else
-        *status = (self->bytes_left & ZKERNEL_ENCODER_BUFFER_MASK)
-                | ZKERNEL_ENCODER_READ_OK;
+    else {
+        *status = ZMTP_V3_ENCODER_READ_OK;
+        if (self->bytes_left >= self->buffer_threshold)
+            *status |= ZMTP_V3_ENCODER_BUFFER_FLAG;
+    }
 
     return 0;
 }
 
-static encoder_status_t
-s_status (encoder_t *base)
+void
+zmtp_v3_encoder_set_buffer_threshold (
+    zmtp_v3_encoder_t *self, size_t buffer_threshold, zmtp_v3_encoder_status_t *status)
 {
-    zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) base;
     assert (self);
 
-    if (self->state == WAITING_FOR_PDU)
-        return ZKERNEL_ENCODER_READY;
-    else
-        return (self->bytes_left & ZKERNEL_ENCODER_BUFFER_MASK)
-              | ZKERNEL_ENCODER_READ_OK;
+    self->buffer_threshold = buffer_threshold;
 
-    return 0;
+    *status = 0;
+    if (self->state == WAITING_FOR_PDU)
+        *status |= ZMTP_V3_ENCODER_READY;
+    else {
+        *status |= ZMTP_V3_ENCODER_READ_OK;
+        if (self->bytes_left >= self->buffer_threshold)
+            *status |= ZMTP_V3_ENCODER_BUFFER_FLAG;
+    }
 }
 
-static void
-s_destroy (encoder_t **base_p)
+void
+zmtp_v3_encoder_destroy (zmtp_v3_encoder_t **base_p)
 {
     if (*base_p) {
         zmtp_v3_encoder_t *self = (zmtp_v3_encoder_t *) *base_p;
@@ -201,15 +207,6 @@ s_destroy (encoder_t **base_p)
         *base_p = NULL;
     }
 }
-
-static struct encoder_ops ops = {
-    .encode = s_encode,
-    .read = s_read,
-    .buffer = s_buffer,
-    .advance = s_advance,
-    .status = s_status,
-    .destroy = s_destroy
-};
 
 static void
 put_uint64 (uint8_t *ptr, uint64_t n)
--- src/zmtp_v3_encoder.h
+++ src/zmtp_v3_encoder.h
@@ -2,11 +2,44 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*/
 
-#include "encoder.h"
+#include "pdu.h"
+#include "iobuf.h"
 
 #ifndef __ZMTP_V3_ENCODER_H_INCLUDED__
 #define __ZMTP_V3_ENCODER_H_INCLUDED__
 
-encoder_constructor_t zmtp_v3_encoder_create_encoder;
+#define ZMTP_V3_ENCODER_READY           0x01
+#define ZMTP_V3_ENCODER_READ_OK         0x02
+#define ZMTP_V3_ENCODER_BUFFER_FLAG     0x04
+
+typedef struct zmtp_v3_encoder zmtp_v3_encoder_t;
+
+typedef unsigned int zmtp_v3_encoder_status_t;
+
+zmtp_v3_encoder_t *
+    zmtp_v3_encoder_new ();
+
+int
+    zmtp_v3_encoder_putms (
+        zmtp_v3_encoder_t *self, pdu_t *pdu, zmtp_v3_encoder_status_t *status);
+
+int
+    zmtp_v3_encoder_read (
+        zmtp_v3_encoder_t *self, iobuf_t *iobuf, zmtp_v3_encoder_status_t *status);
+
+int
+    zmtp_v3_encoder_buffer (
+        zmtp_v3_encoder_t *self, const void **buffer, size_t *buffer_size);
+
+int
+    zmtp_v3_encoder_advance (
+        zmtp_v3_encoder_t *self, size_t n, zmtp_v3_encoder_status_t *status);
+
+void
+    zmtp_v3_encoder_set_buffer_threshold (
+        zmtp_v3_encoder_t *self,size_t buffer_threshold, zmtp_v3_encoder_status_t *status);
+
+void
+    zmtp_v3_encoder_destroy (zmtp_v3_encoder_t **base_p);
 
 #endif
